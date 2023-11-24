023c4e5c916004fb93bc800efff75f9f2e920779
Quansnake <horvathbercel10@gmail.com>
2023-11-24 12:54:52 +0100

    n

--- Models/BackGroundModel.cs
+++ Models/BackGroundModel.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Verseny.Models
 {
     public class BackGroundModel
     {
+
         public void ReadGames(string filename)
         {
+            StreamReader reader = new StreamReader("jatekok.txt");
 
         }
     }
